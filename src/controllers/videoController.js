23517246f0cca320f26a525ae6d9cf9bad66e120
Jonghyeon Choi <whd617@naver.com>
2024-02-28 01:50:31 +0900

    chore/README.md 파일 생성 - chore/Pug파일 생성 - chore/routers폴더 및 파일 생성 - chore/controller 폴더 및 파일 생성

---
+++ src/controllers/videoController.js
@@ -0,0 +1,10 @@
+export const trending = (req, res) => res.render('home');
+
+export const see = (req, res) => res.render('watch');
+
+export const edit = (req, res) => res.render('edit');
+export const search = (req, res) => res.send('Search');
+export const deleteVideo = (req, res) => {
+  return res.send('Delete');
+};
+export const upload = (req, res) => res.send('Upload');
--- src/server.js
+++ src/server.js
@@ -1,10 +1,20 @@
-import express from 'express';
+import express from 'express'; // express도 export default로 구성되어져 있다.
+import morgan from 'morgan';
+import globalRouter from './routers/globalRouter';
+import userRouter from './routers/userRouter';
+import videoRouter from './routers/videoRouter';
 
 const PORT = 4000;
 
 const app = express(); // express function을 사용하면 express application을 생성해준다.
+const logger = morgan('dev');
 
-app.get('/', () => console.log('Somebody is trying to go home.')); // app이라는 application에 누군가가 어떤 route로, 이 경우엔 home으로 get request를 보낸다면, 반응하는 callback을 추가하는 것이다.
+app.set('view engine', 'pug'); // 뷰 엔진을 pug로 설정
+app.set('views', process.cwd() + '/src/views'); // 뷰 엔진의 디렉토리 경로 변경하기
+app.use(logger);
+app.use('/', globalRouter);
+app.use('/videos', videoRouter);
+app.use('/users', userRouter);
 
 const handleListening = () =>
   console.log(`Server listenting on port http://localhost:${PORT}🚀`);
