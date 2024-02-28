9bd90bc1034e534be2b50934650f9b9090057692
Jonghyeon Choi <whd617@naver.com>
2024-02-29 00:16:08 +0900

    chore/API method(get, post) 공부 -chore/mongodb 연결

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+import connect from './db';
 import express from 'express'; // express도 export default로 구성되어져 있다.
 import morgan from 'morgan';
 import globalRouter from './routers/globalRouter';
@@ -6,12 +7,16 @@ import videoRouter from './routers/videoRouter';
 
 const PORT = 4000;
 
+connect();
+
 const app = express(); // express function을 사용하면 express application을 생성해준다.
 const logger = morgan('dev');
 
 app.set('view engine', 'pug'); // 뷰 엔진을 pug로 설정
 app.set('views', process.cwd() + '/src/views'); // 뷰 엔진의 디렉토리 경로 변경하기
 app.use(logger);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // 위치 중요!
 app.use('/', globalRouter);
 app.use('/videos', videoRouter);
 app.use('/users', userRouter);
