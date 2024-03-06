50527efc21815c29648be71336b223361517c851
Jonghyeon Choi <whd617@naver.com>
2024-03-07 00:49:01 +0900

    feat/로그아웃 기능 구현 -feat/user Profile 수정 기능 구현 -feat/user Password 수정 기능 구현 -feat/image file upload 기능 구현 -feat/video file upload 기능 구현 -feat/user Profile 조회 기능 구현 중

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,7 +1,48 @@
+import multer from 'multer';
+
 export const localMiddleware = (req, res, next) => {
-  console.log(req.sessionID);
-  res.locals.loggedIn = Boolean(req.session.loggedIn);
-  res.locals.siteName = 'Wetube';
-  res.locals.loggedInUser = req.session.user;
-  next();
+   console.log(req.sessionID);
+   res.locals.loggedIn = Boolean(req.session.loggedIn);
+   res.locals.siteName = 'Wetube';
+   // loggedInUser는 req.session.user인데, 이게 undefined일 수 가 있다.
+   res.locals.loggedInUser = req.session.user || {};
+   next();
 };
+
+// 로그인하지 않는 사용자가 로그인을 해야만 접근하려는 가능한 페이지를 보호하는 middleware
+// 사용자가 로그인 돼 있지 않은 걸 확인하며, 로그인 페이지로 redirect하게함
+// 사용자가 로그인돼 있다면 request를 계속하도록 할거고
+// controller에서는 user를 req에서 찾을 수 있다.
+export const protectorMiddleware = (req, res, next) => {
+   if (req.session.loggedIn) {
+      next();
+   } else {
+      return res.redirect('/login');
+   }
+};
+
+// 로그인 돼 있지 않은 사람들만 접근 할 수 있게하는 middlware
+export const publicOnlyMiddleware = (req, res, next) => {
+   if (!req.session.loggedIn) {
+      return next();
+   } else {
+      return res.redirect('/');
+   }
+};
+
+// 파일 업로드 middleware
+// 사용자가 보낸 파일을 uploads 폴더에 저장하도록 설정된 middleware
+export const avatarUpload = multer({
+   dest: 'uploads/avatars',
+   limits: {
+      fileSize: 3000000,
+   },
+});
+
+// fileSize 최대 10000 bytes
+export const videoUpload = multer({
+   dest: 'uploads/videos/',
+   limits: {
+      fileSize: 10000000,
+   },
+});
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,20 +1,35 @@
 import express from 'express';
 import {
-  edit,
-  remove,
-  logout,
-  see,
-  startGithubLogin,
-  finishGithubLogin,
+   getEdit,
+   postEdit,
+   logout,
+   see,
+   startGithubLogin,
+   finishGithubLogin,
+   getChangePassword,
+   postChangePassword,
 } from '../controllers/userController';
+import {
+   protectorMiddleware,
+   publicOnlyMiddleware,
+   avatarUpload,
+} from '../middlewares';
 
 const userRouter = express.Router();
 
-userRouter.get('/logout', logout);
-userRouter.get('/edit', edit);
-userRouter.get('/delete', remove);
-userRouter.get('/github/start', startGithubLogin);
-userRouter.get('/github/finish', finishGithubLogin);
+userRouter.get('/logout', protectorMiddleware, logout);
+userRouter
+   .route('/edit')
+   .all(protectorMiddleware)
+   .get(getEdit)
+   .post(avatarUpload.single('avatar'), postEdit);
+userRouter
+   .route('/change-password')
+   .all(protectorMiddleware)
+   .get(getChangePassword)
+   .post(postChangePassword);
+userRouter.get('/github/start', publicOnlyMiddleware, startGithubLogin);
+userRouter.get('/github/finish', publicOnlyMiddleware, finishGithubLogin);
 userRouter.get('/:id', see);
 
 export default userRouter;
